b95780c22204333f7877774733d3286814a70fbf
unknown <geethu.sameela@edu.devinci.fr>
2024-02-03 13:41:05 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+ #!/usr/bin/env python
+ from distutils.core import setup
+ setup(
+ name='{Contribute-to-the-Open-Source-Movement}',
+ version='1.0',
+ author='{geethu-sameela}',
+ license='MIT',
+ long_description=open('README.md').read(),
+ )
